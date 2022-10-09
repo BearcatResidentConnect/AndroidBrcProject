71969266d13d6f5a3021e3f88600af8b3dcbafbb
Ramu Devarashetti <ramudevarashetti@gmail.com>
2022-10-08 20:18:00 -0500

    [Ramu Devarashetti]  Added a Rental Register Page

---
+++ app/src/main/java/com/example/brcandroid/RentalRegister.java
@@ -0,0 +1,14 @@
+package com.example.brcandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RentalRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rental_register);
+    }
+}
