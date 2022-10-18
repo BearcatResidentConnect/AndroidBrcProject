b4116fc63413b0e471de5cbcc55b3e0e9c6b5bd8
Narendarkatta <k.narendarreddy111@gmail.com>
2022-10-18 13:37:47 -0500

    Adding recycler view adapter classes

--- app/src/main/java/com/example/brcandroid/UserApplications.java
+++ app/src/main/java/com/example/brcandroid/UserApplications.java
@@ -1,14 +1,28 @@
 package com.example.brcandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class UserApplications extends AppCompatActivity {
 
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_applications);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+
+        RecyclerAdapter adapter =new RecyclerAdapter(this);
+
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
