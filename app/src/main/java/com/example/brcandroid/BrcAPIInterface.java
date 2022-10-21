d2b1c7401f097a1096671cd6775cd1b943675401
Ramu Devarashetti <ramudevarashetti@gmail.com>
2022-10-21 11:57:29 -0500

    Added brcGetAPI and brcPostAPI methods

--- app/src/main/java/com/example/brcandroid/BrcAPIInterface.java
+++ app/src/main/java/com/example/brcandroid/BrcAPIInterface.java
@@ -7,22 +7,37 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONObject;
+
 public class BrcAPIInterface {
-    private final String repoURL;
+    private final String basuUrl;
     private final RequestQueue requestQueue;
 
-    public BrcAPIInterface(Context context, String repoOwnerUsername, String repoName) {
-        String GITHUB_BASE_URL = "https://api.github.com/repos";
-        this.repoURL = String.format("%s/%s/%s", GITHUB_BASE_URL, repoOwnerUsername, repoName);
+    public BrcAPIInterface(Context context, String basuUrl) {
+
+        this.basuUrl = basuUrl;
         this.requestQueue = Volley.newRequestQueue(context);
+
     }
 
-    public void getLatestReleaseVersion(BrcAPIResponse apiResponse) {
+    public void brcGetAPI(String url, BrcAPIResponse apiResponse) {
         JsonObjectRequest jsonObjectRequest =
                 new JsonObjectRequest(
                         Request.Method.GET,
-                        repoURL + "/releases/latest",
+                        url,
                         null,
+                        apiResponse::onSuccess,
+                        apiResponse::onFailure
+                );
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    public void brcPostAPI(JSONObject requestBody, String url, BrcAPIResponse apiResponse) {
+        JsonObjectRequest jsonObjectRequest =
+                new JsonObjectRequest(
+                        Request.Method.POST,
+                        url,
+                        requestBody,
                         apiResponse::onSuccess,
                         apiResponse::onFailure
                 );
