d08694eff9cb6bfff6dec40a256fd5db0498c919
Ramu Devarashetti <ramudevarashetti@gmail.com>
2022-10-21 17:52:33 -0500

    Updated HTTP Client APIs Implemented Login from API

--- app/src/main/java/com/example/brcandroid/MainActivity.java
+++ app/src/main/java/com/example/brcandroid/MainActivity.java
@@ -4,10 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,10 +59,44 @@ public class MainActivity extends AppCompatActivity {
         // whether the entered data is valid or if any fields are left blank.
         isAllFieldsChecked = CheckAllFields();
 
-        if (isAllFieldsChecked) {
-            Intent i = new Intent(MainActivity.this, UserHome.class);
-            startActivity(i);
-        }
+       try{
+           EditText userNameET = findViewById(R.id.idEdtUserName);
+           EditText userPasswordET = findViewById(R.id.idEdtPassword);
+
+           String userName = userNameET.getText().toString();
+           String password = userPasswordET.getText().toString();
+
+           BrcAPIInterface api = new BrcAPIInterface(MainActivity.this, "http://192.168.1.35:5000/api");
+           JSONObject requestBody = new JSONObject();
+           //requestBody.put("content-type", "application/json");
+           requestBody.put("user_name", userName);
+           requestBody.put("password", password);
+            api.brcPostAPI("/users/auth",requestBody, new BrcAPIResponse() {
+                @Override
+                public void onSuccess(JSONObject response) {
+
+                    Toast.makeText(MainActivity.this,"Log in Successful ", Toast.LENGTH_SHORT).show();
+                    //
+                    Intent i = new Intent(MainActivity.this, UserHome.class);
+                    startActivity(i);
+
+                }
+
+                @Override
+                public void onFailure(Exception exception) {
+
+                    Toast.makeText(MainActivity.this, "Some Error", Toast.LENGTH_SHORT).show();
+                    Log.v("Exception: ", "Exception " + exception);
+
+                }
+
+            });
+
+       }catch (Exception e) {
+           e.printStackTrace();
+           Toast.makeText(MainActivity.this, "Unexpected error occurred", Toast.LENGTH_SHORT).show();
+       }
+
     }
 
     public void onClickRegister(View v){
