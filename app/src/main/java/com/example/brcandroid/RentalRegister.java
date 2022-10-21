250dd5ea043374d1881049b9c20dd802aff0efbc
Narendarkatta <k.narendarreddy111@gmail.com>
2022-10-21 18:00:40 -0500

    adding validation in rental register

--- app/src/main/java/com/example/brcandroid/RentalRegister.java
+++ app/src/main/java/com/example/brcandroid/RentalRegister.java
@@ -3,12 +3,17 @@ package com.example.brcandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class RentalRegister extends AppCompatActivity {
+
+    private EditText userNameEdt, emailEdt, passwordEdt, cpasswordEdt;
+    boolean isAllFieldsChecked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rental_register);
     }
+
 }
