574b0ab7c6d6ce1f4ebf7161695f7841287f9d42
Ramu Devarashetti <ramudevarashetti@gmail.com>
2022-10-21 11:49:49 -0500

    Added BRC Python API response class

--- app/src/main/java/com/example/brcandroid/BrcAPIResponse.java
+++ app/src/main/java/com/example/brcandroid/BrcAPIResponse.java
@@ -1,2 +1,11 @@
-package com.example.brcandroid;public class BrcAPIResponse {
+package com.example.brcandroid;
+
+import org.json.JSONObject;
+
+public abstract class BrcAPIResponse {
+    public abstract void onSuccess(JSONObject response);
+    public abstract void onFailure(Exception exception);
 }
+
+
+
